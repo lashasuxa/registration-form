b992ddaca7d93a487368bdf7e06dc166eeac5c2e
lashasuxa <lashasukhashvili1995@gmail.com>
2023-02-25 23:26:21 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ *   Mini User List
+ *   App Description
+ * - Add onSubmit handler on form
+ * - Validate input values
+ * - Show error message if input is invalid
+ * - Hide error message on input change
+ * - Create user with input values
+ * - Render user card in users container
+ * - Allow remove && star/unstar functionality on user card
+ * - Allow creation of user card with pre-defined array
+ */
