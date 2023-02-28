095a7b9a989258a6d933ae89494e8b8a3e4bb9b1
lashasuxa <lashasukhashvili1995@gmail.com>
2023-02-27 19:08:32 -0500

    js edited

--- index.js
+++ index.js
@@ -7,8 +7,7 @@
  * - Hide error message on input change
  * - Create user with input values
  * - Render user card in users container
- * - Allow remove && star/unstar functionality on user card
- * - Allow creation of user card with pre-defined array
+
  */
 
 const form = document.getElementById("my-form");
@@ -16,6 +15,22 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const imageInput = document.getElementById("image");
 const error = document.getElementById("error");
+const users = document.getElementById("users");
+
+const inputs = [nameInput, emailInput, imageInput];
+
+for (let i = 0; i < inputs.length; i++) {
+  const input = inputs[i];
+  input.addEventListener("input", function () {
+    if (!hasAppError) return; //early exit
+    error.textContent = "";
+    error.classList.remove("error");
+    hasAppError = false;
+  });
+}
+
+// aplication variables
+let hasAppError = false;
 
 form.addEventListener("submit", onsubmit);
 
@@ -48,7 +63,28 @@ function onsubmit(event) {
   if (!isValidValues) {
     error.textContent = "Please fill inputs with valid values";
     error.classList.add("error");
+    hasAppError = true;
+    return; //early exit
   }
+
+  const userContainer = document.createElement("div");
+  userContainer.classList.add("userContainer");
+  userContainer.id = userEmail;
+
+  const userNameTag = document.createElement("h1");
+  userNameTag.textContent = userName;
+  userContainer.appendChild(userNameTag);
+
+  const userEmailTag = document.createElement("h4");
+  userEmailTag.textContent = userEmail;
+  userContainer.appendChild(userEmailTag);
+
+  const userImageTag = document.createElement("img");
+  userImageTag.src = userImage;
+  userImageTag.classList.add("userImage");
+  userContainer.appendChild(userImageTag);
+
+  users.appendChild(userContainer);
 
   console.log(isValidValues);
 }
